e9c9c2f1730bfff3a42742539296370d07917d7d
Jose Lezama <joseglego@gmail.com>
2019-06-30 10:29:55 -0400

    pchore: include leaflet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import './App.css'
 
 function App() {
+  const position = [-33.4372517, -70.6330319]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className='app__title'>TheTop - BurgerMap!</h1>
+      <div className='map-container'>
+        <Map className='map-container__map' center={position} zoom={12}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          />
+          <Marker position={position}>
+            <Popup>Yo</Popup>
+          </Marker>
+        </Map>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
